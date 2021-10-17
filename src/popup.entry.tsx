1d0b4c2b616da4d28da957b75af2cd90191af07c
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-10-17 13:34:56 +0330

    chore: add html entries

---
+++ src/popup.entry.tsx
@@ -0,0 +1,3 @@
+console.log('popup');
+
+export {};
--- webpack.monkey.js
+++ webpack.monkey.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const glob = require('glob');
 
@@ -8,9 +9,35 @@ function getEntries() {
   return Object.fromEntries(entries);
 }
 
+function setHtmlPlugins(webpackConfig) {
+  const htmlFiles = glob.sync('src/*.html');
+  const htmlPlugins = htmlFiles.map((file) => {
+    return new HtmlWebpackPlugin({
+      chunks: [path.basename(file, '.html')],
+      filename: path.basename(file),
+      template: path.resolve(file),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    });
+  });
+  // replace item[0] (default html plugin) with html plugins
+  webpackConfig.plugins.splice(0, 1, ...htmlPlugins);
+}
+
 module.exports = function (webpackConfig, isDevelopment) {
   if (!isDevelopment) {
     webpackConfig.entry = getEntries();
+    setHtmlPlugins(webpackConfig);
     delete webpackConfig.optimization.splitChunks;
     webpackConfig.optimization.runtimeChunk = false;
   }
