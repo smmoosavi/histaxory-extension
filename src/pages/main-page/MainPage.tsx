991aea6e3ffeddb69874937e84aed9f7542cacab
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2021-10-18 09:12:19 +0330

    snapp ....

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,5 +1,11 @@
-import { PropsWithChildren, useMemo, useState } from 'react';
+import { useWire, useWireValue } from '@forminator/react-wire';
+import { t } from '@lingui/macro';
+import { Box, Stack } from '@mui/material';
+import { PropsWithChildren, useCallback, useMemo } from 'react';
 import { createRemoteDriver } from 'src/shared/drivers';
+import { Item } from 'src/shared/drivers/type';
+import { SubmitButton } from 'src/shared/submit-button';
+import { useSubmit, useSubmitWire } from 'src/shared/submit-wire';
 
 interface OwnProps {}
 
@@ -8,17 +14,43 @@ export type Props = PropsWithChildren<OwnProps>;
 export function MainPage() {
   const tabId = Number(window.location.search.split('?')[1]);
   let driver = useMemo(() => createRemoteDriver(tabId), [tabId]);
-  const [loading, setLoading] = useState(false);
+  const items$ = useWire<Item[]>(null, []);
+  const goToStoryPage$ = useSubmitWire();
+  useSubmit(
+    goToStoryPage$,
+    useCallback(async () => {
+      await driver.goToHistoryPage();
+    }, [driver]),
+  );
+  const loadItems$ = useSubmitWire();
+  useSubmit(
+    loadItems$,
+    useCallback(async () => {
+      const res = await driver.loadItems();
+      console.log('res load items', res);
+      items$.setValue(res.items);
+    }, [driver, items$]),
+  );
+  const items = useWireValue(items$);
   return (
-    <div>
-      <button
-        onClick={() => {
-          setLoading(true);
-          driver.goToHistoryPage().then(() => setLoading(false));
-        }}
-      >
-        {loading ? 'loading' : 'go to history page'}
-      </button>
-    </div>
+    <Box m={2}>
+      <Stack spacing={2}>
+        <Box>
+          <SubmitButton
+            fullWidth
+            variant="contained"
+            submit$={goToStoryPage$}
+          >{t`Go to history page`}</SubmitButton>
+        </Box>
+        <Box>
+          <SubmitButton
+            fullWidth
+            variant="contained"
+            submit$={loadItems$}
+          >{t`Load trips`}</SubmitButton>
+        </Box>
+        <Box>item counts: {items.length}</Box>
+      </Stack>
+    </Box>
   );
 }
